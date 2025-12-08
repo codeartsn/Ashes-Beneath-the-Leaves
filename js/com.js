a88d4152272db273c9f7a76e2631b2440e109c6d
codeartsn <coderxcodes@gmail.com>
2025-12-08 17:34:29 +0530

    comm.js

--- js/com.js
+++ js/com.js
@@ -5,14 +5,13 @@ input.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
         event.preventDefault();
 
-        const original = title.textContent;   // save old title
+        const original = title.textContent;
         title.textContent = "Thanks for sharing!";
 
-        // change back after 2 seconds
         setTimeout(() => {
             title.textContent = original;
         }, 1500);
 
-        input.value = ""; // optional: clear textarea
+        input.value = "";
     }
 });
